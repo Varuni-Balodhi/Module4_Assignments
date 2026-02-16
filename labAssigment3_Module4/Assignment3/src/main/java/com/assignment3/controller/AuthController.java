47613a48c2ec8c711e752063177798a6e335dbb0
Varuni-Balodhi <VE00YN375@GLOBALYMC.COM>
2026-02-16 15:18:04 +0530

    Add files via upload

---
+++ labAssigment3_Module4/Assignment3/src/main/java/com/assignment3/controller/AuthController.java
@@ -0,0 +1,5 @@
+package com.assignment3.controller;
+
+public class AuthController {
+
+}
---
+++ labAssigment3_Module4/Assignment3/src/main/java/com/assignment3/entity/Book.java
@@ -0,0 +1,14 @@
+package com.assignment3.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+	private Integer id;
+	private String title;
+	private String author;
+}
